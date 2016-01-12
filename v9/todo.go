e1b6389f601e4844aaecf7808ea8389f45d0a316
mehiar <mehiar.osu@gmail.com>
2016-01-12 20:43:11 +0000

    added required json fields

--- v9/todo.go
+++ v9/todo.go
@@ -7,6 +7,11 @@ type Todo struct {
 	Name      string    `json:"name"`
 	Completed bool      `json:"completed"`
 	Due       time.Time `json:"due"`
+	Image     string    `json:"image"`
+	Command   string    `json:"command"`
+	Cpus      string    `json:"cpus"`
+	Mem       string    `json:"mem"`
+	Status    string    `json:"status"`
 }
 
 type Todos []Todo
